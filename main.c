1ec06b711582bec62fd351e7f6a1fa210ef3f6f3
Nikita <librixfgitmail@gmail.com>
2025-04-20 19:00:00 +0500

    Перепиманна основная логика работы с заметками, также изменился формат хранения заметок.

--- main.c
+++ main.c
@@ -1,97 +1,104 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 1000
 
-size_t i;
 
-int selection(int n, char *line);
-char output_line(char *line);
-char deletion_line(char *line);
+int selection(char *line);
+char output_line(char **mas, int c);
+char deletion_line(char **mas, int c);
+int add_to_mass(char *line, char **mas, int c);
 
 int main(void) {
-    int n, s;
+    int c = 0;
     char *str = NULL;
 
     str = (char *)malloc(MAX * sizeof(char));
-
     if(str == NULL) {
         printf("Неизвестная ошибка!");
         return EXIT_FAILURE;
     }
 
-    n = s = 0;
-    deletion_line(str);
-    
-    while (true)
-    {
-        selection(n, str);
-    }
+    c = selection(str);
 
     free(str);
     return EXIT_SUCCESS;
 }
 
-int selection(int n, char *line) {
-    extern size_t i;
-    size_t max = MAX;
-
-    printf("\nВведите одно из чисел:\n1. Запись новой заметки.\n2. Вывод записи.\n3. Стирание.\nЧисло: ");
-
-    n = getchar();
-    getchar();
-
-    if (n == '1')
-    {
-    printf("Запись пошла!\n");
-
-    i = getline(&line, &max, stdin);
-
-    printf("\nЗаписанно: %s", line);  
-          
-    } else if (n == '2')
-    {
-        output_line(line);
-    } else if (n == '3')
-    {
-        deletion_line(line);
-    } else if (n == '4')
-    {
-        return EXIT_SUCCESS;
-    }
-    
-    return EXIT_SUCCESS;
-}
-
-char output_line(char *line) {
-    int n,c;
-    n = c = 0;
-    
-    if (line[0] != '\0')
-    {
-        printf("\n%s", line);
-    } else
-    {
-        printf("Ничего нет!");
-    }
-    
-    printf("\nВывод зевершён!\n");
-    return EXIT_SUCCESS;
-}
-
-char deletion_line(char *line) {
-    extern size_t i;
+int selection(char *line) {
     int n;
+    size_t i;
+    size_t max = MAX;
+    char *mas_chast[MAX];
+    static int c = 0;
+    
+    while (true) {
+        printf("Введите одно из чисел:\n1. Запись новой заметки.\n2. Вывод записи.\n3. Стирание.\nЧисло: ");
 
-    for (n = 0; n <= i; n++)
+        n = getchar();
+        getchar();
+
+        if (n == '1')
+        {
+        printf("Запись пошла!\n");
+
+        i = getline(&line, &max, stdin);
+        c = add_to_mass(line, mas_chast, c);
+
+        } else if (n == '2')
+        {
+            output_line(mas_chast, c);
+        } else if (n == '3')
+        {
+            deletion_line(mas_chast, c);
+        } else if (n == '4')
+        {
+            break;
+        }
+    }
+    
+    return EXIT_SUCCESS;
+}
+
+char output_line(char **mas, int c) {
+    int n;
+    
+    printf("Заметки:\n");
+    for (n = 0; n < c; n++)
     {
-        line[n] = '\0';
+        printf("%s\n", mas[n]);
     }
 
-    n = i = 0;
+    return EXIT_SUCCESS;
+}
+
+char deletion_line(char **mas, int c) {
+    int n;
+    for (n = 0; c > n; n++)
+    {
+        free(mas[n]);
+        mas[n] = NULL;
+    }
 
     printf("Очистка зевершена!\n");
 
     return EXIT_SUCCESS;
+}
+
+int add_to_mass(char *line, char **mas, int c) {
+    
+    size_t len = strlen(line);
+
+    mas[c] = (char *)malloc((len + 1) * sizeof(char));
+    if (mas[c] == NULL)
+    {
+        printf("Неизвестная ошибка!");
+        return EXIT_FAILURE;
+    }
+
+    strncpy(mas[c], line, len);
+    mas[c][len + 1] = '\0';
+    return c + 1;
 }
